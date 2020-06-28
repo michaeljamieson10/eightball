ab3b4e2edfca727662ea58c5c372e892eb146e00
michael <michael@michaels-MBP.fios-router.home>
2020-06-28 17:40:18 -0400

    finished 8 ball project

---
+++ src/EightBall.js
@@ -0,0 +1,50 @@
+import React,{ useState } from 'react';
+import './EightBall.css';
+
+
+const messages = [
+    { msg: "It is certain.", color: "green" },
+    { msg: "It is decidedly so.", color: "green" },
+    { msg: "Without a doubt.", color: "green" },
+    { msg: "Yes - definitely.", color: "green" },
+    { msg: "You may rely on it.", color: "green" },
+    { msg: "As I see it, yes.", color: "green" },
+    { msg: "Most likely.", color: "green" },
+    { msg: "Outlook good.", color: "green" },
+    { msg: "Yes.", color: "green" },
+    { msg: "Signs point to yes.", color: "goldenrod" },
+    { msg: "Reply hazy, try again.", color: "goldenrod" },
+    { msg: "Ask again later.", color: "goldenrod" },
+    { msg: "Better not tell you now.", color: "goldenrod" },
+    { msg: "Cannot predict now.", color: "goldenrod" },
+    { msg: "Concentrate and ask again.", color: "goldenrod" },
+    { msg: "Don't count on it.", color: "red" },
+    { msg: "My reply is no.", color: "red" },
+    { msg: "My sources say no.", color: "red" },
+    { msg: "Outlook not so good.", color: "red" },
+    { msg: "Very doubtful.", color: "red" },
+  ]
+// let chosen = choice(messages)
+function EightBall() {
+
+    let randomNum = Math.floor(Math.random() * messages.length)
+    const [color, setColor] = useState('black')
+    const [message, setMessage] = useState('Think of a Question')
+    function changeColorAndMessage(){
+        randomNum = Math.floor(Math.random() * messages.length)
+        setMessage(messages[randomNum].msg)
+        setColor(messages[randomNum].color)
+    }
+  
+    return (
+      <div className="EightBall" >
+          
+            <div className="EightBall-circle" style={{backgroundColor: color}} onClick={changeColorAndMessage}>
+                <span className="EightBall-text">{ message }</span>
+            </div>
+      </div>
+    );
+  }
+  
+  export default EightBall;
+  
